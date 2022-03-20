4065340878b65fb20d91985dbee3731fe9ff8ce2
jiyoungmerong <gjwldud0719@daum.net>
2022-03-20 23:22:10 +0900

    백준 15894 수학은 체육과목입니다

---
+++ 220320/baek_15894.java
@@ -0,0 +1,10 @@
+package al;
+
+import java.io.*;
+
+public class baek_15894 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		System.out.print(Long.parseLong(br.readLine()) * 4);
+	}
+}
